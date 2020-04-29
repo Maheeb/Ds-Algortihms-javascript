debbb8fe62c068825ee7002a9a277d06ca2ee056
Maheeb <maheeb009@gmail.com>
2020-04-29 21:58:35 +0600

    Palindrome

--- palindrome/index.js
+++ palindrome/index.js
@@ -7,6 +7,16 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+function palindrome(str) {
+    // let rev = str.split('').reverse().join('');
+    // if (str === rev){
+    //     return true
+    // }
+    // return false
+
+   return str.split('').every((char,i)=>{
+       return char === str[str.length-i-1]
+   })
+}
 
 module.exports = palindrome;
