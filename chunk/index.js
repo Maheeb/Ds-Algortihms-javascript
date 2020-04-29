da6ae7608018fcbf52107b85933bf6312ad8838a
Maheeb <maheeb009@gmail.com>
2020-04-30 02:31:45 +0600

    Chunking Array

--- chunk/index.js
+++ chunk/index.js
@@ -8,6 +8,14 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    const chunked =[];
+    let index =0
+    while(index<array.length){
+        chunked.push(array.slice(index,index+size))
+        index = index +size
+    }
+    return chunked
+}
 
 module.exports = chunk;
