4fc9352a5e777b358ca8e484d248b5a442a35047
Maheeb <maheeb009@gmail.com>
2020-04-29 23:32:34 +0600

    Integer Reverse

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,17 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    //
+    let rev = n.toString().split('').reverse().join('');
+    let res = parseInt(rev);
+
+    if(Math.sign(n)=== -1){
+         return res * -1;
+    }
+    return res;
+
+
+}
 
 module.exports = reverseInt;
